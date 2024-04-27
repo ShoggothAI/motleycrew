dbf16d1afb062e84a878869654240d17a7294afa
User <serfintw@yadnex.ru>
2024-04-27 12:21:36 +0300

    add set_update_cache_if_exists function in caching package

--- motleycrew/caсhing/__init__.py
+++ motleycrew/caсhing/__init__.py
@@ -1,2 +1,2 @@
-from .caching import enable_cache, disable_cache, set_cache_location, set_strong_cache
+from .caching import enable_cache, disable_cache, set_cache_location, set_strong_cache, set_update_cache_if_exists
 from .http_cache import CACHE_WHITELIST, CACHE_BLACKLIST

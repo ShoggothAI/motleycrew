1a1e09927aec50acc27f9c52e066ceb674f33f9f
whimo <whimox@gmail.com>
2024-04-27 14:51:15 +0400

    Research agent: first working implementation

--- examples/research_agent/answer_orchestrator.py
+++ examples/research_agent/answer_orchestrator.py
@@ -1,92 +1,68 @@
-from llama_index.core.graph_stores.types import GraphStore
-
 import logging
-import sys
 import kuzu
 
-from langchain.prompts import PromptTemplate
-from langchain.tools import Tool
-
-from motleycrew import MotleyTool
 from motleycrew.storage import MotleyGraphStore
-from motleycrew.tool.llm_tool import LLMTool
 
 from question_struct import Question
-from question_generator import QuestionGeneratorTool
-from question_generator import QuestionGeneratorToolInput
 from question_answerer import AnswerSubQuestionTool
-
-logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 
 class AnswerOrchestrator:
-    def __init__(self, storage: MotleyGraphStore, query_tool: MotleyTool):
+    def __init__(self, storage: MotleyGraphStore, answer_length: int):
         self.storage = storage
-        self.query_tool = query_tool
-        self.question_answering_tool = AnswerSubQuestionTool(graph=self.storage)
+        self.question_answering_tool = AnswerSubQuestionTool(graph=self.storage, answer_length=answer_length)
 
     def get_unanswered_available_questions(self) -> list[Question]:
-        query = "MATCH (n1:{}) WHERE n1.answer IS NULL AND NOT (n1)-[:{}]->(:{}) RETURN n1;".format(
-            self.storage.node_table_name, self.storage.rel_table_name, self.storage.node_table_name
-        )
+        query = (
+            "MATCH (n1:{}) "
+            "WHERE n1.answer IS NULL AND n1.context IS NOT NULL "
+            "AND NOT EXISTS {{MATCH (n1)-[]->(n2:{}) "
+            "WHERE n2.answer IS NULL AND n2.context IS NOT NULL}} "
+            "RETURN n1"
+        ).format(self.storage.node_table_name, self.storage.node_table_name)
 
-        query_result = self.storage.run_query(query)
+        query_result = self.storage.run_cypher_query(query)
         return [Question.deserialize(row[0]) for row in query_result]
 
-    def __call__(self):
+    def __call__(self) -> Question | None:
         last_question = None
 
         while True:
-            questions = self.storage.get_unanswered_available_questions()
+            questions = self.get_unanswered_available_questions()
+            logging.info("Available questions: %s", questions)
+
             if not len(questions):
-                return last_question
+                logging.info("All questions answered!")
+                break
             else:
                 last_question = questions[0]
-                answerer.invoke({"question": last_question})
-        return graph.retrieve(last_question.id)
+                logging.info("Running answerer for question %s", last_question)
+                self.question_answering_tool.invoke({"question": last_question})
+
+        if not last_question:
+            logging.warning("Nothing to answer!")
+            return
+
+        return Question.deserialize(self.storage.get_entity(last_question.id))
 
 
 if __name__ == "__main__":
     from pathlib import Path
-    import shutil
     from dotenv import load_dotenv
     from motleycrew.storage import MotleyKuzuGraphStore
+    from motleycrew.common.utils import configure_logging
 
     load_dotenv()
+    configure_logging(verbose=True)
+
     here = Path(__file__).parent
     db_path = here / "research_db"
-    shutil.rmtree(db_path, ignore_errors=True)
 
     db = kuzu.Database(db_path)
     storage = MotleyKuzuGraphStore(
         db, node_table_schema={"question": "STRING", "answer": "STRING", "context": "STRING"}
     )
 
-    query_tool = MotleyTool.from_langchain_tool(
-        Tool.from_function(
-            func=lambda question: [
-                "Germany has consisted of many different states over the years",
-                "The capital of France has moved in 1815, from Lyons to Paris",
-                "France actually has two capitals, one in the north and one in the south",
-            ],
-            name="Query Tool",
-            description="Query the library for relevant information.",
-            args_schema=QuestionGeneratorToolInput,
-        )
-    )
-
-    orchestrator = KnowledgeGainingOrchestrator(storage=storage, query_tool=query_tool)
-    orchestrator(query="Why did Arjuna kill his step-brother?", max_iter=5)
-
-
-def answer_orchestrator(graph: GraphStore):
-    last_question = None
-    answerer = AnswerSubQuestionTool(graph=graph)
-    while True:
-        questions = graph.get_unanswered_available_questions()
-        if not len(questions):
-            return last_question
-        else:
-            last_question = questions[0]
-            answerer.invoke({"question": last_question})
-    return graph.retrieve(last_question.id)
+    orchestrator = AnswerOrchestrator(storage=storage, answer_length=30)
+    result = orchestrator()
+    print(result)

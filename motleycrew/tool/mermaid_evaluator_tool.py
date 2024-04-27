744bcbe728ae1ffc6f83e04f6ee937d7b244ea4b
Egor Kraev <egor.kraev@gmail.com>
2024-04-27 12:48:48 +0200

    mermaid evaluator final draft

--- motleycrew/tool/mermaid_evaluator_tool.py
+++ motleycrew/tool/mermaid_evaluator_tool.py
@@ -6,16 +6,46 @@ import io
 import tempfile
 from typing import Optional
 
+from langchain_core.pydantic_v1 import create_model, Field
+from langchain_core.tools import Tool
+from motleycrew.tool import MotleyTool
+
+
+class MermaidEvaluatorTool(MotleyTool):
+    def __init__(self, format: Optional[str] = "svg"):
+        def eval_mermaid_partial(mermaid_code: str):
+            return eval_mermaid(mermaid_code, format)
+
+        langchain_tool = Tool.from_function(
+            func=eval_mermaid_partial,
+            name="Mermaid Evaluator Tool",
+            description="Evaluates Mermaid code and returns the output as a BytesIO object.",
+            args_schema=create_model(
+                "MermaidEvaluatorToolInput",
+                mermaid_code=(str, Field(description="The Mermaid code to evaluate.")),
+            ),
+        )
+        super().__init__(langchain_tool)
+
 
 def eval_mermaid(mermaid_code: str, format: Optional[str] = "svg") -> io.BytesIO:
     with tempfile.NamedTemporaryFile(delete=True, mode="w+", suffix=".mmd") as temp_in:
         temp_in.write(mermaid_code)
         temp_in.flush()  # Ensure all data is written to disk
 
+        if format in ["md", "markdown"]:
+            raise NotImplementedError(
+                "Markdown format is not yet supported in this wrapper."
+            )
+        assert format in [
+            "svg",
+            "png",
+            "pdf",
+        ], "Invalid format specified, must be svg, png, or pdf."
         out_file = f"output.{format}"
 
         # Prepare the command to call the mermaid CLI
-        full_code = f"C:/Users/Egor/AppData/Roaming/npm/mmdc -i {temp_in.name} -o {out_file} -b transparent"
+        full_code = f"mmdc -i {temp_in.name} -o {out_file} -b transparent"
 
         try:
             # Execute the command
@@ -55,5 +85,7 @@ if __name__ == "__main__":
         E[End] --> F[End]
     """
 
-    out = eval_mermaid(mermaid_code)
-    print(out)
+    out1 = eval_mermaid(mermaid_code)
+    tool = MermaidEvaluatorTool()
+    out2 = tool.invoke({"mermaid_code": mermaid_code})
+    print(out2)

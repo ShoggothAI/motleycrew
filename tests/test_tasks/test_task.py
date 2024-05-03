21172b959d3bfaab8f56304a250fc75463abae62
whimo <whimox@gmail.com>
2024-05-03 22:13:35 +0400

    Fix deduplicating dependent tasks + tests

--- tests/test_tasks/test_task.py
+++ tests/test_tasks/test_task.py
@@ -41,14 +41,14 @@ class TestSetUpstream:
     def test_set_upstream_sets_upstream(self, task1, task2):
         task2.set_upstream(task1)
 
-        assert task1.upstream_tasks == set()
-        assert task2.upstream_tasks == {task1}
+        assert task1.upstream_tasks == []
+        assert task2.upstream_tasks == [task1]
 
     def test_set_upstream_sets_downstreams(self, task1, task2):
         task2.set_upstream(task1)
 
-        assert task1.downstream_tasks == {task2}
-        assert task2.downstream_tasks == set()
+        assert task1.downstream_tasks == [task2]
+        assert task2.downstream_tasks == []
 
     def test_rshift_returns_left(self, task1, task2):
         result = task1 >> task2
@@ -58,28 +58,28 @@ class TestSetUpstream:
     def test_rshift_sets_downstream(self, task1, task2):
         task1 >> task2
 
-        assert task1.downstream_tasks == {task2}
-        assert task2.downstream_tasks == set()
+        assert task1.downstream_tasks == [task2]
+        assert task2.downstream_tasks == []
 
     def test_rshift_sets_upstream(self, task1, task2):
         task1 >> task2
 
-        assert task1.upstream_tasks == set()
-        assert task2.upstream_tasks == {task1}
+        assert task1.upstream_tasks == []
+        assert task2.upstream_tasks == [task1]
 
     def test_rshift_set_multiple_downstream(self, task1, task2, task3):
         task1 >> [task2, task3]
 
-        assert task1.downstream_tasks == {task2, task3}
-        assert task2.downstream_tasks == set()
-        assert task3.downstream_tasks == set()
+        assert task1.downstream_tasks == [task2, task3]
+        assert task2.downstream_tasks == []
+        assert task3.downstream_tasks == []
 
     def test_rshift_set_multiple_upstream(self, task1, task2, task3):
         task1 >> [task2, task3]
 
-        assert task1.upstream_tasks == set()
-        assert task2.upstream_tasks == {task1}
-        assert task3.upstream_tasks == {task1}
+        assert task1.upstream_tasks == []
+        assert task2.upstream_tasks == [task1]
+        assert task3.upstream_tasks == [task1]
 
     def test_sequence_on_left_returns_sequence(self, task1, task2, task3):
         result = [task1, task2] >> task3
@@ -89,21 +89,21 @@ class TestSetUpstream:
     def test_sequence_on_left_sets_downstream(self, task1, task2, task3):
         [task1, task2] >> task3
 
-        assert task1.downstream_tasks == {task3}
-        assert task2.downstream_tasks == {task3}
-        assert task3.downstream_tasks == set()
+        assert task1.downstream_tasks == [task3]
+        assert task2.downstream_tasks == [task3]
+        assert task3.downstream_tasks == []
 
     def test_sequence_on_left_sets_upstream(self, task1, task2, task3):
         [task1, task2] >> task3
 
-        assert task1.upstream_tasks == set()
-        assert task2.upstream_tasks == set()
-        assert task3.upstream_tasks == {task1, task2}
+        assert task1.upstream_tasks == []
+        assert task2.upstream_tasks == []
+        assert task3.upstream_tasks == [task1, task2]
 
     def test_deduplicates(self, task1, task2):
         task1 >> [task2, task2]
 
-        assert task1.downstream_tasks == {task2}
+        assert task1.downstream_tasks == [task2]
 
     def test_error_on_direct_dependency_cycle(self, task1):
         with pytest.raises(TaskDependencyCycleError):

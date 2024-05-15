18e9426fcf56e6adb4153866e79595d532ee9cfa
User <serfintw@yadnex.ru>
2024-05-15 14:54:09 +0300

    add --minimal arg in run_integration_tests

--- tests/run_integration_tests.py
+++ tests/run_integration_tests.py
@@ -43,6 +43,17 @@ IPYNB_INTEGRATION_TESTS = {
     "single_openai_tools_react_ipynb": "examples/single_openai_tools_react.ipynb",
 }
 
+MINIMAL_INTEGRATION_TESTS = {}
+
+MINIMAL_IPYNB_INTEGRATION_TESTS = {
+    "delegation_crewai_ipynb": "examples/delegation_crewai.ipynb",
+    "image_generation_crewai_ipynb": "examples/image_generation_crewai.ipynb",
+    "math_crewai_ipynb": "examples/math_crewai.ipynb",
+    "single_crewai_ipynb": "examples/single_crewai.ipynb",
+    "single_llama_index_ipynb": "examples/single_llama_index.ipynb",
+    "single_openai_tools_react_ipynb": "examples/single_openai_tools_react.ipynb",
+}
+
 DEFAULT_CACHE_DIR = Path(__file__).parent / "itest_cache"
 DEFAULT_GOLDEN_DIR = Path(__file__).parent / "itest_golden_data"
 TIKTOKEN_CACHE_DIR_NAME = "tiktoken_cache"
@@ -69,6 +80,12 @@ def get_args_parser():
         "--update-golden",
         action="store_true",
         help="Update reference data together with the cache",
+    )
+    parser.add_argument(
+        "--minimal",
+        default=False,
+        action="store_true",
+        help="Run minimal tests"
     )
 
     return parser
@@ -138,10 +155,11 @@ def unset_tiktoken_cache_dir(old_value: Optional[str] = None):
     os.environ.pop(TIKTOKEN_CACHE_DIR_ENV_VAR, None)
 
 
-def build_ipynb_integration_tests() -> dict:
+def build_ipynb_integration_tests(is_minimal: bool = False) -> dict:
     """Build and return dict of ipynb integration tests functions"""
     test_functions = {}
-    for test_name, nb_path in IPYNB_INTEGRATION_TESTS.items():
+    tests = MINIMAL_IPYNB_INTEGRATION_TESTS if is_minimal else IPYNB_INTEGRATION_TESTS
+    for test_name, nb_path in tests.items():
         if not os.path.exists(nb_path):
             logging.info("Ipynb test notebook {} not found".format(test_name))
             continue
@@ -156,11 +174,16 @@ def run_integration_tests(
     golden_dir: str,
     update_golden: bool = False,
     test_name: Optional[str] = None,
+    is_minimal: bool = False
 ):
     failed_tests = {}
 
-    integration_tests = copy(INTEGRATION_TESTS)
-    integration_tests.update(build_ipynb_integration_tests())
+    if is_minimal:
+        integration_tests = copy(MINIMAL_INTEGRATION_TESTS)
+    else:
+        integration_tests = copy(INTEGRATION_TESTS)
+
+    integration_tests.update(build_ipynb_integration_tests(is_minimal))
 
     old_tiktoken_cache_dir = set_tiktoken_cache_dir(cache_dir)
 
@@ -221,6 +244,7 @@ def main():
         golden_dir=args.golden_dir,
         update_golden=args.update_golden,
         test_name=args.test_name,
+        is_minimal=args.minimal
     )
 
 

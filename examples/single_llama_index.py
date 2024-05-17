5e99664843dbb8a50c416f532df2d2a9e03ee9c4
Egor Kraev <egor.kraev@gmail.com>
2024-05-17 18:31:49 +0200

    Integrate notebooks into doc structure

--- examples/single_llama_index.py
+++ examples/single_llama_index.py
@@ -2,9 +2,11 @@ from dotenv import load_dotenv
 from langchain_community.tools import DuckDuckGoSearchRun
 
 
+from llama_index.core.composability import QASummaryQueryEngineBuilder
 from motleycrew import MotleyCrew
 from motleycrew.agents.llama_index import ReActLlamaIndexMotleyAgent
 from motleycrew.common.utils import configure_logging
+from motleycrew.tasks import SimpleTaskRecipe
 
 
 def main():
@@ -21,7 +23,8 @@ def main():
     crew = MotleyCrew()
 
     # Create tasks for your agents
-    task = crew.create_simple_task(
+    task = SimpleTaskRecipe(
+        crew=crew,
         name="produce comprehensive analysis report on AI advancements",
         description="""Conduct a comprehensive analysis of the latest advancements in AI in 2024.
       Identify key trends, breakthrough technologies, and potential industry impacts.

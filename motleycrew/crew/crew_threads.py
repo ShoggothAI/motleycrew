73356b2792449b7a7e5f53342f2d1a79ea5d7019
whimo <whimox@gmail.com>
2024-06-03 21:24:34 +0400

    Refactor crew async methods

--- motleycrew/crew/crew_threads.py
+++ motleycrew/crew/crew_threads.py
@@ -1,56 +1,59 @@
 """Thread pool module for running agents"""
 
+from typing import TYPE_CHECKING, Tuple, Any, List
 import threading
 from queue import Queue
 from enum import Enum
-from typing import Tuple, Any, List
+
+from langchain_core.runnables import Runnable
 
 from motleycrew.common import Defaults
 
+if TYPE_CHECKING:
+    from motleycrew import Task
+    from motleycrew.tasks import TaskUnit
 
-class InvokeThreadState(Enum):
-    PROCESS = "process"
+
+class TaskUnitThreadState(Enum):
+    BUSY = "busy"
     WAITING = "waiting"
-    STOP = "stop"
+    EXITED = "exited"
 
 
-class InvokeThread(threading.Thread):
+SENTINEL = object()  # sentinel object for closing threads
 
+
+class TaskUnitThread(threading.Thread):
     def __init__(self, input_queue: Queue, output_queue: Queue, *args, **kwargs):
-        """The thread class for running tasks
+        """The thread class for running task units
 
         Args:
-            input_queue (Queue): queue of tasks to complete
-            output_queue (Queue): queue of completed tasks
+            input_queue (Queue): queue of task units to complete
+            output_queue (Queue): queue of completed task units
             *args:
             **kwargs:
         """
         self.input_queue = input_queue
         self.output_queue = output_queue
-        self._state = InvokeThreadState.WAITING
-        self._is_close = False
-        super(InvokeThread, self).__init__(*args, **kwargs)
+        self._state = TaskUnitThreadState.WAITING
+
+        super(TaskUnitThread, self).__init__(*args, **kwargs)
 
     @property
     def state(self):
         return self._state
 
-    def stop(self):
-        """Plans to stop the execution cycle when the next task is received"""
-        self.input_queue.put(None)
-        self._is_close = True
-
     def run(self) -> None:
         while True:
-            running_data = self.input_queue.get()
+            run_data = self.input_queue.get()
+            self._state = TaskUnitThreadState.BUSY
 
-            self._state = InvokeThreadState.PROCESS
-
-            if running_data is None or self._is_close:
-                self._state = InvokeThreadState.STOP
+            if run_data is SENTINEL:
+                self._state = TaskUnitThreadState.EXITED
+                self.input_queue.task_done()
                 break
 
-            agent, task, unit = running_data
+            agent, task, unit = run_data
             try:
                 result = agent.invoke(unit.as_dict())
             except Exception as e:
@@ -58,32 +61,31 @@ class InvokeThread(threading.Thread):
             else:
                 self.output_queue.put((task, unit, result))
             finally:
-                self._state = InvokeThreadState.WAITING
+                self._state = TaskUnitThreadState.WAITING
+                self.input_queue.task_done()
 
 
-class InvokeThreadPool:
-
-    def __init__(self, num_threads: int = None):
-        """The thread storage class for performing tasks
+class TaskUnitThreadPool:
+    def __init__(self, num_threads: int = Defaults.DEFAULT_NUM_THREADS):
+        """The thread pool class for performing task units
 
         Args:
-            num_threads (int): number of threads being created default Defaults.DEFAULT_NUM_THREADS
+            num_threads (int): number of threads to create
         """
-        self.num_threads = num_threads or Defaults.DEFAULT_NUM_THREADS
+        self.num_threads = num_threads
 
-        self.lock = threading.Lock()
         self.input_queue = Queue()
         self.output_queue = Queue()
 
         self._threads = []
         for i in range(self.num_threads):
-            thread = InvokeThread(self.input_queue, self.output_queue)
+            thread = TaskUnitThread(self.input_queue, self.output_queue)
             thread.start()
             self._threads.append(thread)
-        self._in_process_tasks = []
+        self._task_units_in_progress = []
 
-    def put(self, agent: "Runnable", task: "Task", unit: "TaskUnit"):
-        """Adds a task to the queue for execution
+    def add_task_unit(self, agent: Runnable, task: "Task", unit: "TaskUnit"):
+        """Adds a task unit to the queue for execution
 
         Args:
             agent (Runnable):
@@ -93,36 +95,40 @@ class InvokeThreadPool:
         Returns:
 
         """
-        self._in_process_tasks.append((task, unit))
+        self._task_units_in_progress.append((task, unit))
         self.input_queue.put((agent, task, unit))
 
-    def get_completed_tasks(self) -> List[Tuple["Task", "TaskUnit", Any]]:
-        """Returns a list of completed tasks
+    def get_completed_task_units(self) -> List[Tuple["Task", "TaskUnit", Any]]:
+        """Returns a list of completed task units with their results
 
         Returns:
-            List[Tuple[Task, TaskUnit, Any]]: list of completed tasks  or empty list
+            List[Tuple[Task, TaskUnit, Any]]: list of triplets of task, task unit, and result
         """
         completed_tasks = []
         while not self.output_queue.empty():
-
             task_result = self.output_queue.get()
             if isinstance(task_result, Exception):
                 raise task_result
 
             task, unit, result = task_result
             completed_tasks.append((task, unit, result))
-            self._in_process_tasks.remove((task, unit))
+            self._task_units_in_progress.remove((task, unit))
         return completed_tasks
 
-    def close(self):
-        """Closes running threads"""
+    def wait_and_close(self):
+        """Wait for all task units to complete and close the threads"""
         for t in self._threads:
-            t.stop()
+            if t.is_alive():
+                self.input_queue.put(SENTINEL)
+        self.input_queue.join()
+
+        for t in self._threads:
+            t.join()
 
     def is_completed(self) -> bool:
-        """Returns the result of checking the completion and returning all tasks
+        """Returns whether all task units have been completed
 
         Returns:
             bool:
         """
-        return not bool(self._in_process_tasks)
+        return not bool(self._task_units_in_progress)

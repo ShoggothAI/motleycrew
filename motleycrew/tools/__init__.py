1b658e70d5f815d141635bf78ffb93cf40a5a08f
User <serfintw@yadnex.ru>
2024-06-06 10:03:39 +0300

    remove AiderTool from tools init

--- motleycrew/tools/__init__.py
+++ motleycrew/tools/__init__.py
@@ -5,4 +5,3 @@ from motleycrew.tools.image.dall_e import DallEImageGeneratorTool
 from .llm_tool import LLMTool
 from .mermaid_evaluator_tool import MermaidEvaluatorTool
 from .python_repl import PythonREPLTool
-from .aider_tool import AiderTool

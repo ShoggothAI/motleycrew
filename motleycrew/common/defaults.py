d36ba4f5ed28e3bcf32dc54a747304f12d6d190f
whimo <whimox@gmail.com>
2024-06-19 09:27:44 +0300

    Switch default model to gpt-4o & update tests

--- motleycrew/common/defaults.py
+++ motleycrew/common/defaults.py
@@ -18,7 +18,7 @@ class Defaults:
 
     """
     DEFAULT_LLM_FAMILY = LLMFamily.OPENAI
-    DEFAULT_LLM_NAME = "gpt-4-turbo"
+    DEFAULT_LLM_NAME = "gpt-4o"
     DEFAULT_LLM_TEMPERATURE = 0.0
     LLM_MAP = {}
 

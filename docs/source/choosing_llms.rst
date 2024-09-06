07806f5047b3a0ebceaaaa09f0826df62013926b
Artemy Belousov <whimox@gmail.com>
2024-09-06 13:19:35 +0400

    Support various LLM providers (Ollama, Groq, Together...) + docs (#75)
    * Support Replicate-hosted LLMs
    
    * Add support for Together, Groq, and Ollama LLMs in Langchain and LlamaIndex integration.
    
    * Replace `OllamaLLM` with `ChatOllama` in `langchain_ollama_llm` function.
    
    * Fix import path for ChatOllama in langchain_ollama_llm function.
    
    * LLM-related docs & refactoring

---
+++ docs/source/choosing_llms.rst
@@ -0,0 +1,108 @@
+Choosing LLMs
+====================
+
+Generally, the interaction with an LLM is up to the agent implementation.
+However, as motleycrew integrates with several agent frameworks, there is some common ground for how to choose LLMs.
+
+
+Providing an LLM to an agent
+----------------------------
+
+In general, you can pass a specific LLM to the agent you're using.
+
+.. code-block:: python
+
+    from motleycrew.agents.langchain import ReActToolCallingMotleyAgent
+    from langchain_openai import ChatOpenAI
+
+    llm = ChatOpenAI(model="gpt-4o", temperature=0)
+    agent = ReActToolCallingMotleyAgent(llm=llm, tools=[...])
+
+
+The LLM class depends on the agent framework you're using.
+That's why we have an ``init_llm`` function to help you set up the LLM.
+
+.. code-block:: python
+
+    from motleycrew.common.llms import init_llm
+    from motleycrew.common import LLMFramework, LLMProvider
+
+    llm = init_llm(
+        llm_framework=LLMFramework.LANGCHAIN,
+        llm_provider=LLMProvider.ANTHROPIC,
+        llm_name="claude-3-5-sonnet-20240620",
+        llm_temperature=0
+    )
+    agent = ReActToolCallingMotleyAgent(llm=llm, tools=[...])
+
+
+The currently supported frameworks (:py:class:`motleycrew.common.enums.LLMFramework`) are:
+
+- :py:class:`Langchain <motleycrew.common.enums.LLMFramework.LANGCHAIN>` for Langchain-based agents from Langchain, CrewAI, motelycrew etc.
+- :py:class:`LlamaIndex <motleycrew.common.enums.LLMFramework.LLAMA_INDEX>` for LlamaIndex-based agents.
+
+The currently supported LLM providers (:py:class:`motleycrew.common.enums.LLMProvider`) are:
+
+- :py:class:`OpenAI <motleycrew.common.enums.LLMProvider.OPENAI>`
+- :py:class:`Anthropic <motleycrew.common.enums.LLMProvider.ANTHROPIC>`
+- :py:class:`Groq <motleycrew.common.enums.LLMProvider.GROQ>`
+- :py:class:`Together <motleycrew.common.enums.LLMProvider.TOGETHER>`
+- :py:class:`Replicate <motleycrew.common.enums.LLMProvider.REPLICATE>`
+- :py:class:`Ollama <motleycrew.common.enums.LLMProvider.OLLAMA>`
+
+Please raise an issue if you need to add support for another LLM provider.
+
+
+Default LLM
+-----------
+
+At present, we default to OpenAI's latest ``gpt-4o`` model for our agents,
+and rely on the user to set the `OPENAI_API_KEY` environment variable.
+
+You can control the default LLM as follows:
+
+.. code-block:: python
+
+    from motleycrew.common import Defaults
+    Defaults.DEFAULT_LLM_PROVIDE = "the_new_default_LLM_provider"
+    Defaults.DEFAULT_LLM_NAME = "name_of_the_new_default_model_from_the_provider"
+
+
+Using custom LLMs
+-----------------
+
+To use a custom LLM provider to use as the default or via the ``init_llm`` function,
+you need to make sure that for all the frameworks you're using (currently at most Langchain, LlamaIndex),
+the `LLM_MAP` has an entry for the LLM provider, for example as follows:
+
+.. code-block:: python
+
+    from motleycrew.common import LLMProvider
+    from motleycrew.common.llms import LLM_MAP
+
+    LLM_MAP[(LLMFramework.LANGCHAIN, "MyLLMProvider")] = my_langchain_llm_factory
+    LLM_MAP[(LLMFramework.LLAMA_INDEX, "MyLLMProvider")] = my_llamaindex_llm_factory
+
+Here each llm factory is a function with a signature
+``def llm_factory(llm_name: str, llm_temperature: float, **kwargs)`` that returns the model object for the relevant framework.
+
+For example, this is the built-in OpenAI model factory for Langchain:
+
+.. code-block:: python
+
+    def langchain_openai_llm(
+        llm_name: str = Defaults.DEFAULT_LLM_NAME,
+        llm_temperature: float = Defaults.DEFAULT_LLM_TEMPERATURE,
+        **kwargs,
+    ):
+        from langchain_openai import ChatOpenAI
+
+        return ChatOpenAI(model=llm_name, temperature=llm_temperature, **kwargs)
+
+
+You can also overwrite the `LLM_MAP` values for e.g. the OpenAI models if, for example,
+you want to use an in-house wrapper for Langchain or Llamaindex model adapters
+(for example, to use an internal gateway instead of directly hitting the OpenAI endpoints).
+
+Note that at present, if you use Autogen with motleycrew, you will need to separately control
+the models that Autogen uses, using the Autogen-specific APIs.
--- motleycrew/common/llms.py
+++ motleycrew/common/llms.py
@@ -1,8 +1,8 @@
 """Helper functions to initialize Language Models (LLMs) from different frameworks."""
 
 from motleycrew.common import Defaults
-from motleycrew.common import LLMFamily, LLMFramework
-from motleycrew.common.exceptions import LLMFamilyNotSupported, LLMFrameworkNotSupported
+from motleycrew.common import LLMProvider, LLMFramework
+from motleycrew.common.exceptions import LLMProviderNotSupported
 from motleycrew.common.utils import ensure_module_is_installed
 
 
@@ -74,17 +74,160 @@ def llama_index_anthropic_llm(
     return Anthropic(model=llm_name, temperature=llm_temperature, **kwargs)
 
 
-Defaults.LLM_MAP = {
-    (LLMFramework.LANGCHAIN, LLMFamily.OPENAI): langchain_openai_llm,
-    (LLMFramework.LLAMA_INDEX, LLMFamily.OPENAI): llama_index_openai_llm,
-    (LLMFramework.LANGCHAIN, LLMFamily.ANTHROPIC): langchain_anthropic_llm,
-    (LLMFramework.LLAMA_INDEX, LLMFamily.ANTHROPIC): llama_index_anthropic_llm,
+def langchain_replicate_llm(
+    llm_name: str = Defaults.DEFAULT_LLM_NAME,
+    llm_temperature: float = Defaults.DEFAULT_LLM_TEMPERATURE,
+    **kwargs,
+):
+    """Initialize a Replicate LLM client for use with Langchain.
+
+    Args:
+        llm_name: Name of the LLM in Replicate API.
+        llm_temperature: Temperature for the LLM.
+    """
+    from langchain_community.llms import Replicate
+
+    model_kwargs = kwargs.pop("model_kwargs", {})
+    model_kwargs["temperature"] = llm_temperature
+
+    return Replicate(model=llm_name, model_kwargs=model_kwargs, **kwargs)
+
+
+def llama_index_replicate_llm(
+    llm_name: str = Defaults.DEFAULT_LLM_NAME,
+    llm_temperature: float = Defaults.DEFAULT_LLM_TEMPERATURE,
+    **kwargs,
+):
+    """Initialize a Replicate LLM client for use with LlamaIndex.
+
+    Args:
+        llm_name: Name of the LLM in Replicate API.
+        llm_temperature: Temperature for the LLM.
+    """
+    ensure_module_is_installed("llama_index")
+    from llama_index.llms.replicate import Replicate
+
+    return Replicate(model=llm_name, temperature=llm_temperature, **kwargs)
+
+
+def langchain_together_llm(
+    llm_name: str = Defaults.DEFAULT_LLM_NAME,
+    llm_temperature: float = Defaults.DEFAULT_LLM_TEMPERATURE,
+    **kwargs,
+):
+    """Initialize a Together LLM client for use with Langchain.
+
+    Args:
+        llm_name: Name of the LLM in Together API.
+        llm_temperature: Temperature for the LLM.
+    """
+    from langchain_together import ChatTogether
+
+    return ChatTogether(model=llm_name, temperature=llm_temperature, **kwargs)
+
+
+def llama_index_together_llm(
+    llm_name: str = Defaults.DEFAULT_LLM_NAME,
+    llm_temperature: float = Defaults.DEFAULT_LLM_TEMPERATURE,
+    **kwargs,
+):
+    """Initialize a Together LLM client for use with LlamaIndex.
+
+    Args:
+        llm_name: Name of the LLM in Together API.
+        llm_temperature: Temperature for the LLM.
+    """
+    ensure_module_is_installed("llama_index")
+    from llama_index.llms.together import TogetherLLM
+
+    return TogetherLLM(model=llm_name, temperature=llm_temperature, **kwargs)
+
+
+def langchain_groq_llm(
+    llm_name: str = Defaults.DEFAULT_LLM_NAME,
+    llm_temperature: float = Defaults.DEFAULT_LLM_TEMPERATURE,
+    **kwargs,
+):
+    """Initialize a Groq LLM client for use with Langchain.
+
+    Args:
+        llm_name: Name of the LLM in Groq API.
+        llm_temperature: Temperature for the LLM.
+    """
+    from langchain_groq import ChatGroq
+
+    return ChatGroq(model=llm_name, temperature=llm_temperature, **kwargs)
+
+
+def llama_index_groq_llm(
+    llm_name: str = Defaults.DEFAULT_LLM_NAME,
+    llm_temperature: float = Defaults.DEFAULT_LLM_TEMPERATURE,
+    **kwargs,
+):
+    """Initialize a Groq LLM client for use with LlamaIndex.
+
+    Args:
+        llm_name: Name of the LLM in Groq API.
+        llm_temperature: Temperature for the LLM.
+    """
+    ensure_module_is_installed("llama_index")
+    from llama_index.llms.groq import Groq
+
+    return Groq(model=llm_name, temperature=llm_temperature, **kwargs)
+
+
+def langchain_ollama_llm(
+    llm_name: str = Defaults.DEFAULT_LLM_NAME,
+    llm_temperature: float = Defaults.DEFAULT_LLM_TEMPERATURE,
+    **kwargs,
+):
+    """Initialize an Ollama LLM client for use with Langchain.
+
+    Args:
+        llm_name: Name of the LLM in Ollama API.
+        llm_temperature: Temperature for the LLM.
+    """
+    from langchain_ollama.chat_models import ChatOllama
+
+    return ChatOllama(model=llm_name, temperature=llm_temperature, **kwargs)
+
+
+def llama_index_ollama_llm(
+    llm_name: str = Defaults.DEFAULT_LLM_NAME,
+    llm_temperature: float = Defaults.DEFAULT_LLM_TEMPERATURE,
+    **kwargs,
+):
+    """Initialize an Ollama LLM client for use with LlamaIndex.
+
+    Args:
+        llm_name: Name of the LLM in Ollama API.
+        llm_temperature: Temperature for the LLM.
+    """
+    ensure_module_is_installed("llama_index")
+    from llama_index.llms.ollama import Ollama
+
+    return Ollama(model=llm_name, temperature=llm_temperature, **kwargs)
+
+
+LLM_MAP = {
+    (LLMFramework.LANGCHAIN, LLMProvider.OPENAI): langchain_openai_llm,
+    (LLMFramework.LLAMA_INDEX, LLMProvider.OPENAI): llama_index_openai_llm,
+    (LLMFramework.LANGCHAIN, LLMProvider.ANTHROPIC): langchain_anthropic_llm,
+    (LLMFramework.LLAMA_INDEX, LLMProvider.ANTHROPIC): llama_index_anthropic_llm,
+    (LLMFramework.LANGCHAIN, LLMProvider.REPLICATE): langchain_replicate_llm,
+    (LLMFramework.LLAMA_INDEX, LLMProvider.REPLICATE): llama_index_replicate_llm,
+    (LLMFramework.LANGCHAIN, LLMProvider.TOGETHER): langchain_together_llm,
+    (LLMFramework.LLAMA_INDEX, LLMProvider.TOGETHER): llama_index_together_llm,
+    (LLMFramework.LANGCHAIN, LLMProvider.GROQ): langchain_groq_llm,
+    (LLMFramework.LLAMA_INDEX, LLMProvider.GROQ): llama_index_groq_llm,
+    (LLMFramework.LANGCHAIN, LLMProvider.OLLAMA): langchain_ollama_llm,
+    (LLMFramework.LLAMA_INDEX, LLMProvider.OLLAMA): llama_index_ollama_llm,
 }
 
 
 def init_llm(
     llm_framework: str,
-    llm_family: str = Defaults.DEFAULT_LLM_FAMILY,
+    llm_provider: str = Defaults.DEFAULT_LLM_PROVIDER,
     llm_name: str = Defaults.DEFAULT_LLM_NAME,
     llm_temperature: float = Defaults.DEFAULT_LLM_TEMPERATURE,
     **kwargs,
@@ -93,13 +236,13 @@ def init_llm(
 
     Args:
         llm_framework: Framework of the LLM client.
-        llm_family: Family of the LLM.
+        llm_provider: Provider of the LLM.
         llm_name: Name of the LLM.
         llm_temperature: Temperature for the LLM.
     """
 
-    func = Defaults.LLM_MAP.get((llm_framework, llm_family), None)
+    func = LLM_MAP.get((llm_framework, llm_provider), None)
     if func is not None:
         return func(llm_name=llm_name, llm_temperature=llm_temperature, **kwargs)
 
-    raise LLMFamilyNotSupported(llm_framework=llm_framework, llm_family=llm_family)
+    raise LLMProviderNotSupported(llm_framework=llm_framework, llm_provider=llm_provider)

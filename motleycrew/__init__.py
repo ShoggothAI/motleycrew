ec699d3e6f9abe997d570b776fc6a4734d147701
Stefan <1586641+ViStefan@users.noreply.github.com>
2024-09-17 11:21:11 +0000

    populate __version__ (#78)

--- motleycrew/__init__.py
+++ motleycrew/__init__.py
@@ -1,4 +1,10 @@
 """MotleyCrew root package."""
+from importlib import metadata
 
 from .crew import MotleyCrew
 from .tasks import Task
+
+try:
+    __version__ = metadata.version(__package__)
+except metadata.PackageNotFoundError:
+    __version__ = ""

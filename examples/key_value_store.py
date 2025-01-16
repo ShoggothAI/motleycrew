39cd8a63b7cd3d5c1df7755768588b9987288ef8
Egor.Kraev <egor.kraev@transferwise.com>
2025-01-16 10:32:03 +0000

    clean up some test .pkls, revise the objects to be stored in kv-cache

--- examples/key_value_store.py
+++ examples/key_value_store.py
@@ -2,22 +2,28 @@ from typing import List
 
 from motleycrew import MotleyCrew
 from motleycrew.agents.langchain import ReActToolCallingMotleyAgent
-from motleycrew.storage.kv_store_domain import ObjectRetrievalMetadata, ObjectRetrievalResult
+from motleycrew.storage.kv_store_domain import SimpleRetrievableObject
 from motleycrew.tasks import SimpleTask
 from motleycrew.tools import MotleyTool
 
 
 class ObjectInsertionTool(MotleyTool):
-    def run(self, query: str) -> List[ObjectRetrievalMetadata]:
-        metadata = ObjectRetrievalMetadata(id="321", name="test", description="test")
-        response1 = ObjectRetrievalResult(metadata=metadata, payload={"test": 2.35, "blah": 3.14})
-        metadata = ObjectRetrievalMetadata(
-            id="aaa", name="test1", description="another dummy object"
+    def run(self, query: str) -> List[str]:
+        response1 = SimpleRetrievableObject(
+            id="321",
+            name="test",
+            description="test",
+            payload={"test": 2.35, "blah": 3.14},
         )
-        response2 = ObjectRetrievalResult(metadata=metadata, payload={"test": 2.35, "blah": 3.14})
-        self.agent.kv_store[response1.metadata.id] = response1
-        self.agent.kv_store[response2.metadata.id] = response2
-        return [response1.metadata, response2.metadata]
+        response2 = SimpleRetrievableObject(
+            id="aaa",
+            name="test1",
+            description="another dummy object",
+            payload={"test": 2.35, "blah": 3.14},
+        )
+        self.agent.kv_store[response1.id] = response1
+        self.agent.kv_store[response2.id] = response2
+        return [response1.summary, response2.summary]
 
 
 class ObjectFetcherTool(MotleyTool):
@@ -31,13 +37,6 @@ if __name__ == "__main__":
     from motleycrew.common.logging import configure_logging
 
     configure_logging()
-
-    a = ObjectRetrievalMetadata(id="1", name="test", description="test")
-    print(a)
-    b = ObjectRetrievalResult(
-        metadata=ObjectRetrievalMetadata(id="1", name="test", description="test"), payload="test"
-    )
-    print(b)
 
     instructions = """Your job is to first call the Object Insertion Tool and get back from it the metadata
     of some data objects; each metadata will include an id.
